03d7c0e3525f05a1f077774834e04dcfb8e62c59
Julian Galvis Escalante - Ceiba Software <julian.galvis@ceiba.com.co>
2019-02-07 07:47:44 -0500

    first

---
+++ src/main/java/co/com/ceiba/estacionamiento/ceibaestacionamiento/CeibaEstacionamientoApplication.java
@@ -0,0 +1,14 @@
+package co.com.ceiba.estacionamiento.ceibaestacionamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeibaEstacionamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeibaEstacionamientoApplication.class, args);
+	}
+
+}
+
